d82cc557608a138a4db53336034c252699a252cc
Efraín Espada <efrainespada@MacBook-Pro-914.local>
2018-07-15 21:34:47 +0200

    maps working

--- app/src/main/java/com/rotor/chappy/fragments/map/MapFragment.java
+++ app/src/main/java/com/rotor/chappy/fragments/map/MapFragment.java
@@ -1,19 +1,27 @@
 package com.rotor.chappy.fragments.map;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.rotor.chappy.R;
 import com.rotor.chappy.enums.FragmentType;
 import com.rotor.chappy.interfaces.Frag;
 import com.rotor.core.RFragment;
 
-public class MapFragment extends RFragment implements Frag {
+public class MapFragment extends RFragment implements Frag, OnMapReadyCallback {
 
+    private MapView mapView;
+    private GoogleMap map;
 
     @Nullable
     @Override
@@ -22,10 +30,14 @@ public class MapFragment extends RFragment implements Frag {
     }
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    public void onViewCreated(@Nullable View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        if (view != null) {
+            mapView = view.findViewById(R.id.map);
+            mapView.onCreate(savedInstanceState);
+            mapView.getMapAsync(this);
+        }
     }
-
 
 
     @Override
@@ -54,16 +66,41 @@ public class MapFragment extends RFragment implements Frag {
 
     @Override
     public void onResumeView() {
-
+        mapView.onResume();
     }
 
     @Override
     public void onPauseView() {
+        if (mapView != null) {
+            mapView.onPause();
+        }
+    }
 
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
     }
 
     @Override
     public void onBackPressed() {
         getActivity().finish();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        map.getUiSettings().setMyLocationButtonEnabled(false);
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        map.setMyLocationEnabled(true);
     }
 }
